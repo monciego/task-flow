3ad91450e17140687f4a66ad77d333ef5cab6c6e
monciego <jerichobantiquete@gmail.com>
2023-12-26 00:43:45 +0800

    feat: display tasks in table view

--- app/Livewire/Tasks/Tasks.php
+++ app/Livewire/Tasks/Tasks.php
@@ -6,21 +6,10 @@ use Livewire\Component;
 
 class Tasks extends Component
 {
-    public $tasks = [];
-    public $task = "";
-
-    public function addTask() {
-        $this->tasks[] = $this->task;
-
-        // $this->task = '';
-        $this->reset('task');
-    }
-
-    public function mount() {
-        $this->tasks = ["Complete Project Proposal", "Design User Interface", "Implement Livewire Features"];
-    }
     public function render()
     {
-        return view('livewire.tasks.tasks')->layout('layouts.app');
+        return view('livewire.tasks.tasks', [
+            'tasks' => auth()->user()->tasks
+        ])->layout('layouts.app');
     }
 }
--- resources/views/livewire/tasks/tasks.blade.php
+++ resources/views/livewire/tasks/tasks.blade.php
@@ -23,18 +23,70 @@
     </div>
     @endif
 
-    <h2 class="text-black dark:text-white">
-        Tasks
-    </h2>
+    <div class="p-4">
+        <header class="flex items-center justify-between mb-4">
+            <h2 class="text-black text-lg dark:text-white">
+                Tasks
+            </h2>
 
 
-    <a href="{{ route('tasks.create') }}" wire:navigate>Add Task</a>
+            <a href="{{ route('tasks.create') }}" wire:navigate
+                class="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                Add Task
+            </a>
+        </header>
 
-    <ul class="text-black list-disc list-inside dark:text-white">
-        @foreach ($tasks as $task)
-        <li>
-            {{ $task }}
-        </li>
-        @endforeach
-    </ul>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Task Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Priority
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Deadline
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr
+                        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $task->task_name }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $task->status }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $task->priority }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $task->deadline->diffForHumans() }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
 </div>
